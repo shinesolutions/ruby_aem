6ea496a5b169a7573b9350e3109e439fe35b6a86
Cliffano Subagio <cliffano@gmail.com>
2016-07-03 21:56:27 +1000

    Add user set_permission, add_to_group, and change_password implementations.

--- lib/ruby_aem/user.rb
+++ lib/ruby_aem/user.rb
@@ -32,6 +32,32 @@ module RubyAem
       @client.call(self.class, __callee__.to_s, @info)
     end
 
+    def set_permission(permission_path, permission_csv)
+      @info[:permission_path] = permission_path
+      @info[:permission_csv] = permission_csv
+      @client.call(self.class, __callee__.to_s, @info)
+    end
+
+    def add_to_group(group_path, group_name)
+      group_info = @info.dup
+      group_info[:name] = group_name
+      group_info[:path] = group_path
+      result = @client.call(self.class, 'find_authorizable_id', group_info)
+      if result.data
+        @info[:group_path] = group_path
+        @info[:group_authorizable_id] = result.data
+        @client.call(self.class, __callee__.to_s, @info)
+      else
+        result
+      end
+    end
+
+    def change_password(old_password, new_password)
+      @info[:old_password] = old_password
+      @info[:new_password] = new_password
+      @client.call(self.class, __callee__.to_s, @info)
+    end
+
     def find_authorizable_id()
       @client.call(self.class, __callee__.to_s, @info)
     end
--- test/integration/user_spec.rb
+++ test/integration/user_spec.rb
@@ -26,6 +26,69 @@ describe 'User' do
       expect(result.message).to match(/^User someuser exists at \/home\/users\/s\/.+/)
     end
 
+    it 'should succeed permission setting' do
+      result = @user.set_permission('/etc/replication', 'read:true,modify:true')
+      expect(result.is_success?).to be(true)
+      expect(result.message).to eq('Permission read:true,modify:true on path /etc/replication set for user someuser')
+    end
+
+    it 'should succeed admin password change' do
+      result = @user.change_password('admin', 'admin')
+      expect(result.is_success?).to be(true)
+      expect(result.message).to eq('Logged in user password changed')
+    end
+
+    it 'should succeed created user password change' do
+      aem = RubyAem::Aem.new({
+        :username => 'someuser',
+        :password => 'somepassword',
+        :protocol => 'http',
+        :host => 'localhost',
+        :port => 4502,
+        :debug => false
+      })
+      user = aem.user('/home/users/s/', 'someuser')
+      result = user.change_password('somepassword', 'somenewpassword')
+      expect(result.is_success?).to be(true)
+      expect(result.message).to eq('Logged in user password changed')    end
+
+      it 'should succeed being added to a group' do
+
+        # TODO: implement after group implementation is done
+        # # ensure group doesn't exist prior to testing
+        # group_authorizable_id = find_authorizable_id(@sling, '/home/groups/s', 'somegroup')
+        # if group_authorizable_id
+        #   data, status_code, headers = @sling.delete_node_with_http_info(
+        #     path = 'home/groups/s',
+        #     name = group_authorizable_id
+        #   )
+        #   expect(status_code).to eq(204)
+        # end
+        #
+        # # create group
+        # data, status_code, headers = @sling.post_authorizables_with_http_info(
+        #   authorizable_id = 'somegroup',
+        #   intermediate_path = '/home/groups/s',
+        #   {
+        #     :create_group => '',
+        #     :profilegiven_name => 'somegroup'
+        #   }
+        # )
+        # expect(status_code).to eq(201)
+        # group_authorizable_id = find_authorizable_id(@sling, '/home/groups/s', 'somegroup')
+        #
+        # # add user as member to the group
+        # data, status_code, headers = @sling.post_node_rw_with_http_info(
+        #   path = 'home/groups/s',
+        #   name = group_authorizable_id,
+        #   {
+        #     :add_members => 'someuser'
+        #   }
+        # )
+        # expect(status_code).to eq(200)
+
+      end
+
   end
 
 end

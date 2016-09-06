dcce88710d42c1c08b5dc10e8f8a3d24109d0e70
Cliffano Subagio <cliffano@gmail.com>
2016-09-07 01:07:08 +1000

    Add bundle test.

---
+++ test/unit/bundle_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../../lib/ruby_aem/bundle'
+
+describe 'Bundle' do
+  before do
+    @mock_client = double('mock_client')
+  end
+
+  after do
+  end
+
+  describe 'test start' do
+
+    it 'should call client with expected parameters' do
+      expect(@mock_client).to receive(:call).once().with(RubyAem::Bundle, 'start', { :name => 'somebundle' })
+      bundle = RubyAem::Bundle.new(@mock_client, 'somebundle')
+      bundle.start
+    end
+
+  end
+
+  describe 'test stop' do
+
+    it 'should call client with expected parameters' do
+      expect(@mock_client).to receive(:call).once().with(RubyAem::Bundle, 'stop', { :name => 'somebundle' })
+      bundle = RubyAem::Bundle.new(@mock_client, 'somebundle')
+      bundle.stop
+    end
+
+  end
+
+end

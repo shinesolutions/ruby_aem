ce898e6597dd8a82803209e9bd2b3cd915693edb
Cliffano Subagio <cliffano@gmail.com>
2016-09-12 23:48:34 +1000

    100% test coverage.

--- .simplecov
+++ .simplecov
@@ -1,4 +1,4 @@
-SimpleCov.minimum_coverage 0
+SimpleCov.minimum_coverage 100
 SimpleCov.start do
   add_filter 'test/unit/'
   coverage_dir 'doc/coverage'
--- test/unit/client_spec.rb
+++ test/unit/client_spec.rb
@@ -3,26 +3,233 @@ require_relative '../../lib/ruby_aem/client'
 
 describe 'Client' do
   before do
-    @mock_client = double('mock_client')
+    @spec = {
+      'bundle' => {
+        'actions' => {
+          'start' => {
+            'api' => 'console',
+            'operation' => 'postBundle',
+            'params' => {
+              'required' => {
+                'name' => '%{name}',
+                'action' => 'start'
+              }
+            },
+            'responses' => {
+              '200' => {
+                'status' => 'success',
+                'handler' => 'simple',
+                'message' => 'Bundle %{name} started'
+              }
+            }
+          }
+        }
+      }
+    }
   end
 
   after do
   end
 
-  describe 'test create' do
+  describe 'test call' do
 
-    it 'should call client with expected parameters' do
-      expect(@mock_client).to receive(:call).once().with(
-        RubyAem::ConfigProperty,
-        'create',
-        { :name => 'someproperty',
-          :type => 'Boolean',
-          :value => 'true',
-          :run_mode => 'author',
-          :someproperty => 'true',
-          :someproperty_type_hint => 'Boolean' })
-      config_property = RubyAem::ConfigProperty.new(@mock_client, 'someproperty', 'Boolean', 'true')
-      config_property.create('author')
+    it 'should call api send and handle the response' do
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with('somebundle', 'start', {})
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, @spec)
+      client.call(mock_class, 'start', { :name => 'somebundle' })
+    end
+
+    it 'should handle api error' do
+      mock_error = SwaggerAemClient::ApiError.new()
+
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with('somebundle', 'start', {}).and_raise(mock_error)
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, @spec)
+      client.call(mock_class, 'start', { :name => 'somebundle' })
+    end
+
+    it 'should handle optional param without value' do
+      spec = {
+        'bundle' => {
+          'actions' => {
+            'start' => {
+              'api' => 'console',
+              'operation' => 'postBundle',
+              'params' => {
+                'optional' => ['optional1', 'optional2']
+              },
+              'responses' => {
+                '200' => {
+                  'status' => 'success',
+                  'handler' => 'simple',
+                  'message' => 'Bundle %{name} started'
+                }
+              }
+            }
+          }
+        }
+      }
+
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with({ :optional1 => 'value1', :optional2 => 'value2' })
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, spec)
+      client.call(mock_class, 'start', { :name => 'somebundle', :optional1 => 'value1', :optional2 => 'value2' })
+    end
+
+    it 'should handle optional param with non-String value' do
+      spec = {
+        'bundle' => {
+          'actions' => {
+            'start' => {
+              'api' => 'console',
+              'operation' => 'postBundle',
+              'params' => {
+                'optional' => {
+                  'optional1' => true,
+                  'optional2' => false,
+                }
+              },
+              'responses' => {
+                '200' => {
+                  'status' => 'success',
+                  'handler' => 'simple',
+                  'message' => 'Bundle %{name} started'
+                }
+              }
+            }
+          }
+        }
+      }
+
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with({ :optional1 => true, :optional2 => false })
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, spec)
+      client.call(mock_class, 'start', { :name => 'somebundle', :optional1 => true, :optional2 => false })
+    end
+
+    it 'should handle optional param with non-File String value' do
+      spec = {
+        'bundle' => {
+          'actions' => {
+            'start' => {
+              'api' => 'console',
+              'operation' => 'postBundle',
+              'params' => {
+                'optional' => {
+                  'optional1' => 'value1',
+                  'optional2' => 'value2',
+                }
+              },
+              'responses' => {
+                '200' => {
+                  'status' => 'success',
+                  'handler' => 'simple',
+                  'message' => 'Bundle %{name} started'
+                }
+              }
+            }
+          }
+        }
+      }
+
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with({ :optional1 => 'value1', :optional2 => 'value2' })
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, spec)
+      client.call(mock_class, 'start', { :name => 'somebundle', :optional1 => 'value1', :optional2 => 'value2' })
+    end
+
+    it 'should handle optional param with File String value' do
+      spec = {
+        'bundle' => {
+          'actions' => {
+            'start' => {
+              'api' => 'console',
+              'operation' => 'postBundle',
+              'params' => {
+                'optional' => {
+                  'optional1' => '__FILE__'
+                }
+              },
+              'responses' => {
+                '200' => {
+                  'status' => 'success',
+                  'handler' => 'simple',
+                  'message' => 'Bundle %{name} started'
+                }
+              }
+            }
+          }
+        }
+      }
+
+      mock_file = double('mock_file')
+      expect(File).to receive(:open).once().with('/tmp/somepackage-1.2.3.zip', 'r').and_yield(mock_file)
+
+      mock_class = double('mock_class')
+      expect(mock_class).to receive(:name).once().and_return('RubyAem::Bundle')
+
+      mock_api = double('mock_api')
+      expect(mock_api).to receive(:post_bundle_with_http_info).once().with({ :optional1 => mock_file })
+      apis = { :console => mock_api }
+
+      client = RubyAem::Client.new(apis, spec)
+      client.call(mock_class, 'start', { :name => 'somebundle', :optional1 => '__FILE__', :file_path => '/tmp', :package_name => 'somepackage', :package_version => '1.2.3' })
+    end
+
+  end
+
+  describe 'test handle' do
+
+    it 'should return failure result when responses does not contain status code' do
+      data = 'somepayload'
+      status_code = 404
+      headers = nil
+      responses = { 200 => { 'status' => 'success', 'message' => 'Bundle %{name} started' }}
+      info = { :name => 'somebundle' }
+
+      client = RubyAem::Client.new(nil, nil)
+      result = client.handle(data, status_code, headers, responses, info)
+      expect(result.is_failure?).to be(true)
+      expect(result.message).to eq("Unexpected response\nstatus code: 404\nheaders: \ndata: somepayload")
+    end
+
+    it 'should call handler when responses contain status code' do
+      data = 'somepayload'
+      status_code = 200
+      headers = nil
+      responses = { 200 => { 'status' => 'success', 'message' => 'Bundle %{name} started' }}
+      info = { :name => 'somebundle' }
+
+      expect(RubyAem::Handlers).to receive(:send).once().with(nil, 'somepayload', 200, nil, { 'status' => 'success', 'message' => 'Bundle %{name} started' }, { :name => 'somebundle' })
+
+      client = RubyAem::Client.new(nil, nil)
+      client.handle(data, status_code, headers, responses, info)
     end
 
   end

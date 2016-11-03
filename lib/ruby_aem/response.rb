3beda799f555f121eae89e2103578897bc23f439
Cliffano Subagio <cliffano@gmail.com>
2016-11-03 13:43:45 +1100

    Introduce response wrapper.

---
+++ lib/ruby_aem/response.rb
@@ -0,0 +1,38 @@
+=begin
+Copyright 2016 Shine Solutions
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+=end
+
+module RubyAem
+  # Response wraps HTTP response data returned by swagger_aem.
+  class Response
+
+    attr_accessor :status_code
+    attr_accessor :body
+    attr_accessor :headers
+
+    # Initialise a result.
+    #
+    # @param status_code HTTP status code
+    # @param body HTTP response body
+    # @param headers HTTP headers
+    # @return new RubyAem::Response instance
+    def initialize(status_code, body, headers)
+      @status_code = status_code
+      @body = body
+      @headers = headers
+    end
+
+  end
+end
---
+++ test/unit/response_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'spec_helper'
+require_relative '../../lib/ruby_aem/response'
+
+describe 'Response' do
+  before do
+  end
+
+  after do
+  end
+
+  describe 'test initialize' do
+
+    it 'should have status code body and headers' do
+      response = RubyAem::Response.new(200, 'some body', {})
+      expect(response.status_code).to eq(200)
+      expect(response.body).to eq('some body')
+      expect(response.headers).to eq({})
+    end
+
+  end
+
+end

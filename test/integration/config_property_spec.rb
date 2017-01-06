a451ddf45641ca8e565d7a477c72b994184b6bf3
Cliffano Subagio <cliffano@gmail.com>
2017-01-07 09:57:16 +1100

    Add config property integration test for successful scenario.

--- test/integration/config_property_spec.rb
+++ test/integration/config_property_spec.rb
@@ -15,6 +15,21 @@ describe 'ConfigProperty' do
       config_property.create('author')
     end
 
+    it 'should create SSL property correctly when node exists' do
+
+      # ensure node is created new
+      node = @aem.node('/apps/system/config.author', 'org.apache.felix.http')
+      if node.exists().data == true
+        node.delete()
+      end
+      result = node.exists()
+      expect(result.data).to eq(false)
+      result = node.create('sling:OsgiConfig')
+
+      config_property = @aem.config_property('org.apache.felix.https.enable', 'Boolean', true)
+      config_property.create('author')
+    end
+
   end
 
 end

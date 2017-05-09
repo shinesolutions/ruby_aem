d0ee56cae4d712a5925bf33cd23c520d711c6af7
Cliffano Subagio <cliffano@gmail.com>
2017-05-09 16:08:56 +1000

    Add flush agent creation to ensure that there's at least one agent when testing get_agents.

--- test/integration/aem_spec.rb
+++ test/integration/aem_spec.rb
@@ -58,6 +58,13 @@ describe 'Aem' do
   describe 'test get_agents' do
 
     it 'should succeed' do
+
+      # ensure there is at least one agent
+      flush_agent = @aem.flush_agent('author', 'some-flush-agent')
+      if flush_agent.exists().data == false
+        result = flush_agent.create_update('Some Flush Agent Title', 'Some flush agent description', 'http://somehost:8080')
+      end
+
       result = @aem.aem().get_agents('author')
       expect(result.message).to eq('Retrieved agents on author')
       expect(result.data.length).not_to eq(0)

c932b2ee0c94c1413c3e57ccdc9c0b0fc1238b32
Cliffano Subagio <cliffano@gmail.com>
2018-11-13 12:35:34 +1100

    Add simple_message, simple_truststore, and simple_authorizablekeystore.

--- test/unit/handlers/simple_spec.rb
+++ test/unit/handlers/simple_spec.rb
@@ -89,4 +89,91 @@ describe 'Simple Handler' do
       end
     end
   end
+
+  describe 'test simple_message' do
+    it 'should construct result message with true data when the expected message exists' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Some message' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_message('Some message', response, response_spec, call_params)
+      expect(result.data).to be(true)
+      expect(result.message).to eq('Some message')
+      expect(result.response).to be(response)
+    end
+    it 'should construct result message with false data when the expected message does not exist' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Some message' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_message('Some other message', response, response_spec, call_params)
+      expect(result.data).to be(false)
+      expect(result.message).to eq('Some message')
+      expect(result.response).to be(response)
+    end
+  end
+
+  describe 'test simple_truststore' do
+    it 'should construct result message with true data when the expected message exists' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Truststore exists' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_truststore(response, response_spec, call_params)
+      expect(result.data).to be(true)
+      expect(result.message).to eq('Truststore exists')
+      expect(result.response).to be(response)
+    end
+    it 'should construct result message with false data when the expected message does not exist' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Truststore does not exist' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_truststore(response, response_spec, call_params)
+      expect(result.data).to be(false)
+      expect(result.message).to eq('Truststore does not exist')
+      expect(result.response).to be(response)
+    end
+  end
+
+  describe 'test simple_keystore' do
+    it 'should construct result message with true data when the expected message exists' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Keystore exists' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_authorizablekeystore(response, response_spec, call_params)
+      expect(result.data).to be(true)
+      expect(result.message).to eq('Keystore exists')
+      expect(result.response).to be(response)
+    end
+    it 'should construct result message with false data when the expected message does not exist' do
+      data = nil
+      status_code = nil
+      headers = nil
+      response_spec = { 'message' => 'Keystore does not exist' }
+      call_params = {}
+
+      response = RubyAem::Response.new(status_code, data, headers)
+      result = RubyAem::Handlers.simple_authorizablekeystore(response, response_spec, call_params)
+      expect(result.data).to be(false)
+      expect(result.message).to eq('Keystore does not exist')
+      expect(result.response).to be(response)
+    end
+  end
 end

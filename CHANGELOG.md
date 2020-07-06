dc31912d094b6e5281398f119229f35b8d568bba
Michael Bloch <michael.bloch@shinesolutions.com>
2020-07-06 17:55:19 +1000

    Add waiter method for ssl resource

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Add
+- Add waiter method for ssl resource
+
 ## 3.7.0 - 2020-05-21
 ### Added
 - Add new api client for Granite api calls
--- lib/ruby_aem/resources/ssl.rb
+++ lib/ruby_aem/resources/ssl.rb
@@ -71,6 +71,78 @@ module RubyAem
       def get
         @client.call(self.class, __callee__.to_s, @call_params)
       end
+
+      # Check if SSL is enabled via Granite
+      #
+      # @return RubyAem::Result
+      def is_enabled
+        get_ssl = get
+
+        response = get_ssl.response
+        ssl_properties = response.body.properties
+        ssl_enabled = ssl_properties.com_adobe_granite_jetty_ssl_port.is_set
+        ssl_port = ssl_properties.com_adobe_granite_jetty_ssl_port.value
+
+        message = if ssl_enabled.eql?(true)
+                    "HTTPS has been configured on port #{ssl_port}"
+                  else
+                    'HTTPS is not configured'
+                  end
+
+        result = RubyAem::Result.new(message, response)
+        result.data = ssl_enabled
+
+        result
+      end
+
+      # Enable SSL via granite and wait until SSL was enabled
+      #
+      # @param opts hash of the following values:
+      # - keystore_password: Authorizable Keystore password for system-user ssl-service. keystore will be created if it  doesn't exist.
+      # - truststore_password: AEM Global Truststore password. Truststore will be created if it  doesn't exist.
+      # - https_hostname: Hostname for enabling HTTPS listener matching the certificate's common name.
+      # - https_port: Port to listen on for HTTPS requests.
+      # - certificate_file_path:  Path to the HTTPS public certificate file.
+      # - privatekey_file_path: Path to the HTTPS Private Key file.
+      # - _retries: retries library's options (http://www.rubydoc.info/gems/retries/0.0.5#Usage), restricted to max_tries, base_sleep_seconds, max_sleep_seconds
+      # @return RubyAem::Result
+      def enable_wait_until_ready(
+        opts = {
+          force: true,
+          _retries: {
+            max_tries: 30,
+            base_sleep_seconds: 2,
+            max_sleep_seconds: 2
+          }
+        }
+      )
+        opts[:_retries] ||= {}
+        opts[:_retries][:max_tries] ||= 30
+        opts[:_retries][:base_sleep_seconds] ||= 2
+        opts[:_retries][:max_sleep_seconds] ||= 2
+
+        # ensure integer retries setting (Puppet 3 passes numeric string)
+        opts[:_retries][:max_tries] = opts[:_retries][:max_tries].to_i
+        opts[:_retries][:base_sleep_seconds] = opts[:_retries][:base_sleep_seconds].to_i
+        opts[:_retries][:max_sleep_seconds] = opts[:_retries][:max_sleep_seconds].to_i
+
+        # The AEM Granite API to enable SSl is unstable and in some cases it response with response code 0.
+        # This is because the HTTP service is getting restarted during the process of enabling SSL via Granite.
+        # To not end with an error we have to rescue this behaviour and verify afterwards if SSL was enabled.
+        begin
+          result = enable(**opts)
+        rescue RubyAem::Error => e
+          raise StandardError.new(result) unless e.result.response.status_code.zero?
+
+          with_retries(max_tries: opts[:_retries][:max_tries], base_sleep_seconds: opts[:_retries][:base_sleep_seconds], max_sleep_seconds: opts[:_retries][:max_sleep_seconds]) { |retries_count|
+            result = is_enabled
+            message = 'SSL could not be configured or connection timeout please try again.'
+            puts format('SSL Enable check #%<retries_count>d: %<check_result_data>s - %<check_result_message>s', retries_count: retries_count, check_result_data: result.data, check_result_message: result.message)
+            raise StandardError.new(message) if result.data == false
+          }
+        end
+        result
+      end
     end
   end
 end
